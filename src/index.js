9dadb0516fd6e1cc902632656c2f5aa51c1e2db6
umar-abdullah22 <umer4361@gmail.com>
2024-05-25 09:49:16 +0500

    use global css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./global.css";
 import { BrowserRouter } from "react-router-dom";
 const container = document.getElementById("root");
 const root = createRoot(container);

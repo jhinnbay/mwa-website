152a0b40f5ef6039e930b3fb0bc3e89c045681a5
umar-abdullah22 <umer4361@gmail.com>
2024-05-25 09:46:27 +0500

    whitepaper main page

---
+++ src/Whitepaper/Whitepaper.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import Chapter1 from './components/chapter1'
+
+
+const Whitepaper = () => {
+  return (
+    <>
+    <Navbar />
+    <HeroSection />
+    <Chapter1 />
+    </>
+  )
+}
+
+export default Whitepaper

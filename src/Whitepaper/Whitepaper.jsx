9d59ce1cae636ae6fa06df6e433b37138199b03c
umar-abdullah22 <umer4361@gmail.com>
2024-05-25 20:40:33 +0500

    make chapter2 ui

--- src/Whitepaper/Whitepaper.jsx
+++ src/Whitepaper/Whitepaper.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import Chapter1 from './components/chapter1'
+import Chapter2 from './components/chapter2'
 
 
 const Whitepaper = () => {
@@ -10,6 +11,7 @@ const Whitepaper = () => {
     <Navbar />
     <HeroSection />
     <Chapter1 />
+    <Chapter2 />
     </>
   )
 }
---
+++ src/Whitepaper/components/chapter2.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+
+const Chapter2 = () => {
+  return (
+    <>
+      <div className="px-9 pb-16 relative">
+        <div className="py-2.5 border-b border-[#E4E4F0]">
+          <h2 className="max-w-[797px] ml-auto eb-garamond font-bold text-[56px] text-center">
+            Chapter 2: Literacy, Education, & Reformation of The Digital Age
+          </h2>
+        </div>
+        <div className="backdrop-blur-xl relative z-10">
+          <div className="w-[645px] h-[645px] absolute top-20 left-8">
+            <img
+              src="/images/green-circle.png"
+              alt="circle"
+            //   className="w-full h-auto"
+              width={645} height={645}
+            />
+            <img src="/images/toys.gif" alt="toys" className="absolute top-1/3 left-0 right-0 mx-auto" />
+          </div>
+          <div
+            className="max-w-[797px] ml-auto mt-2.5 border border-black pt-6 pb-12 px-4 relative z-10 backdrop-blur-sm"
+          >
+            <p className="roboto font-medium">
+              In an era marked by rapid technological progress, the digital
+              landscape has become a double-edged sword. While the Internet was
+              once hailed as a revolutionary force for global connectivity and
+              knowledge dissemination, it has also given rise to environments
+              that compromise safety and integrity, affecting the mental and
+              emotional well-being of its users. This chapter examines the
+              pressing need for a safe, nurturing online community that promotes
+              literacy and education while addressing socio-economical
+              challenges such as rising suicide rates and systematic racism.
+              <br />
+              <br />
+              The digital age has undeniably broadened access to information,
+              but not without significant caveats. Online spaces are often
+              marred by the presence of botnets and malevolent actors who
+              exploit these platforms for nefarious purposes, ranging from
+              spreading misinformation to harassing individuals. Such
+              environments are not conducive to learning or positive engagement,
+              particularly for younger users. Studies have shown that the
+              absence of regulated, safe online spaces contributes to increased
+              anxiety and isolation among adolescents, factors that are linked
+              to the alarming rise in suicide rates among youth.
+              <br />
+              <br />
+              Moreover, the digital divide accentuates existing inequalities.
+              Systematic racism and socio-economic disparities are mirrored in
+              the access to digital resources and educational opportunities,
+              perpetuating a cycle of exclusion and denial of essential
+              services. Many communities, particularly those of color, find
+              themselves systematically deprived of the benefits of digital
+              literacy, further widening the gap in education and future
+              prospects.
+              <br />
+              <br />
+              Against this backdrop, the Mental Wealth Academy proposes a
+              transformative vision for digital community spaces that are safe,
+              inclusive, and educational. Our approach blends the comfort of a
+              controlled environment with the expansive potential of online
+              learning. By establishing a PG-13 online platform, we aim to
+              create a space where young learners can explore, inquire, and grow
+              without the fear of encountering inappropriate content or harmful
+              interactions. This space will not only focus on academic learning
+              but also on fostering digital citizenship, emphasizing ethics,
+              empathy, and respect in all online engagements.
+              <br />
+              <br />
+              Our educational platform will actively combat systemic issues like
+              racism by ensuring equitable access to resources and
+              representation in content creation. Inclusion will be at the core
+              of our curriculum, reflecting diverse histories and perspectives,
+              and addressing the historical inaccuracies and biases that often
+              infiltrate educational materials. By doing so, we not only educate
+              but also empower learners to recognize and challenge systemic
+              injustices.
+              <br />
+              <br />
+              The Mental Wealth Academy is committed to redefining the digital
+              age as one of safe and meaningful connectivity. Our initiative is
+              a call to action for reshaping online interactions, prioritizing
+              the mental and emotional health of our users while promoting a
+              literate, informed, and conscientious global community. In
+              fostering such an environment, we aim to address and mitigate the
+              impacts of mental health issues and systemic exclusions, paving
+              the way for a more just and educated society.
+              <br />
+              By nurturing these values within our digital confines, we offer a
+              beacon of hope and a model for future online communities, proving
+              that the digital realm can indeed be a place of safety, learning,
+              and mutual respect. This is a necessity, as we move forward into
+              the increasingly digital future.
+            </p>
+          </div>
+        </div>
+        <div className="absolute -bottom-6 right-0">
+          <img src="/images/bg-r-text.png" alt="bg-text" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Chapter2;

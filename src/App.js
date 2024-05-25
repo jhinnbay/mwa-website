dc344453399bd4188f0282dc47a328a9780c8879
umar-abdullah22 <umer4361@gmail.com>
2024-05-25 09:48:37 +0500

    whitepaper route setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home/Home";
+import Whitepaper from "./Whitepaper/Whitepaper";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <div className="main">
         <Routes>
           <Route path="" element={<Home />} />
+          <Route path="/whitepaper" element={<Whitepaper />} />
         </Routes>
       </div>
     </>

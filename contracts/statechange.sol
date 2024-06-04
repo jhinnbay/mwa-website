6b43e818dd0c8923e20123866bc6e72299f5b694
Amaan <amaansayyad@yahoo.com>
2024-06-05 01:34:51 +0530

    Update statechange.sol

--- contracts/statechange.sol
+++ contracts/statechange.sol
@@ -1,1 +1,7 @@
+For Checking the state change visit:
 
+https://github.com/jhinnbay/mwa-website/blob/main/contracts/DAOGovernance.sol
+
+https://github.com/jhinnbay/mwa-website/blob/main/contracts/LibraryManagement.sol
+
+https://github.com/jhinnbay/mwa-website/blob/main/contracts/TreasuryManagement.sol
